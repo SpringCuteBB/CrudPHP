d8e2031abd4a64eb1e0fc144f57dfd5455bbe472
ismael <ismaelvm2005@gmail.com>
2025-02-26 10:49:43 +0100

    Fixed the build Vite command

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,16 @@
 import { defineConfig } from "vite";
 import laravel from "laravel-vite-plugin";
-import tailwindcss from "@tailwindcss/vite";
+// import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
     plugins: [
         laravel({
             input: [
                 "resources/css/app.css",
-                "resources/js/app.ts",
+                "resources/js/app.js",
                 "resources/js/main.js",
-                tailwindcss(),
             ],
             refresh: true,
         }),
     ],
-    resolve: {
-        extensions: [".js", ".ts"],
-    },
 });
